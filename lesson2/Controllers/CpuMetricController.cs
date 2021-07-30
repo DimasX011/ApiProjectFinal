d25ed802c4952710168b5c17fd3b12459ab8a1e2
DimasX011 <Dimon1998daf@mail.ru>
2021-07-30 09:44:08 +0600

    fixed

---
+++ lesson2/Controllers/CpuMetricController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace consoleApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CpuMetricController : ControllerBase
+    {
+      
+
+        [HttpGet("cluster/from/{fromTime}/to/{toTime}")]
+        public IActionResult GetMetricsFromAllCluster([FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
+        {
+            return Ok();
+        }
+
+        [HttpGet("api/metrics/cpu/from/{fromTime}/to/{toTime}")]
+        public IActionResult CPUMetricsController([FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
+        {
+            return Ok();
+        }
+
+    }
+}
